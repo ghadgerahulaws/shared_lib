48d1b24e79284ce87d6347081f646c8ff5960abd
ghadgerahulaws <148465733+ghadgerahulaws@users.noreply.github.com>
2025-03-21 09:35:51 +0530

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,20 +1,11 @@
 def call(String repoUrl, String branch = 'main') {
-    pipeline {
-        agent any
-        stages {
-            stage('Clone Repository') {
-                steps {
-                    script {
-                        checkout([
-                            $class: 'GitSCM',
-                            branches: [[name: "*/${branch}"]],
-                            userRemoteConfigs: [[
-                                url: repoUrl
-                            ]]
-                        ])
-                    }
-                }
-            }
-        }
+    script {
+        checkout([
+            $class: 'GitSCM',
+            branches: [[name: "*/${branch}"]],
+            userRemoteConfigs: [[
+                url: repoUrl
+            ]]
+        ])
     }
 }
